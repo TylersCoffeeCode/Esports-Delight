43a707e0860f780c40377070e88607cf6a39e367
Tyler Coats <coatsctyler@gmail.com>
2023-04-03 01:26:52 -0400

    Added more css to post page

--- esports-delight-client/src/pages/PostPage.jsx
+++ esports-delight-client/src/pages/PostPage.jsx
@@ -19,13 +19,15 @@ const PostPage = () => {
 
   return (
     <div className="bg-[rgb(0,31,63)] w-full h-[100vh] flex flex-wrap justify-center items-end">
-        <div className="w-1/2 h-[93%] overflow-y-scroll">
-            <div>
+        <div className="w-2/4 h-[93%] overflow-y-scroll">
+            <div className=" w-full">
                 {posts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).map((post) => (
-                    <div className="bg-[rgb(17,122,227)] text-white  h-[350px] mb-8 border-4 border-black rounded-lg overflow-hidden">
-                        <img src={post.image} alt="" className=" w-full h-3/4 border-b-4 border-black"/>
+                    <div className="bg-[rgb(17,122,227)] text-white w-full h-[550px] mb-8 border-4 border-black rounded-lg overflow-hidden">
+                        <img src={post.image} alt="" className=" w-full h-4/5 border-b-4 border-black"/>
+                        <div className=" bg-black h-1/5">
                         <h1 className=" text-2xl font-bold ml-2">{post.content}</h1>
                         <h3 className="ml-2">{post.commentAuthor.userName}</h3>
+                        </div>
                     </div>
                 ))}
             </div>
