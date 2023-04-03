c28e326528a33996a01adfb461271bd80546bd3b
Tyler Coats <coatsctyler@gmail.com>
2023-04-03 15:16:16 -0400

    Prep for heroku

--- server.js
+++ server.js
@@ -8,8 +8,12 @@ const PORT = process.env.PORT || 3001
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(`${__dirname}/client/build`))
 
-app.get('/', (req, res) => res.json({message: 'Server Works'}))
+app.get('/', (req, res) => res.json({ message: 'Server Works' }))
 app.use('/api', AppRouter)
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+})
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
