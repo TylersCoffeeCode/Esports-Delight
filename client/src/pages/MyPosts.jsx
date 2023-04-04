d0ccceba9f0690fed314e72bc23db86c2da59f10
Tyler Coats <coatsctyler@gmail.com>
2023-04-04 01:52:20 -0400

    Added edit route and controller, made a popup div for edit page and set up front end client calls for edit

--- client/src/pages/MyPosts.jsx
+++ client/src/pages/MyPosts.jsx
@@ -9,6 +9,13 @@ const MyPosts = () => {
 
     const [posts, setPosts] = useState([])
 
+    const [popup, setPopup] = useState(false)
+    const [popupInfo, setPopupInfo] = useState(null)
+
+    const flipPopup = () => {
+        setPopup(!popup)
+    }
+
     const getPosts = async () => {
         const res = await Client.get(`http://localhost:3001/api/posts/myPosts/${id}`)
         setPosts(res.data.post)
@@ -17,13 +24,49 @@ const MyPosts = () => {
 
     const handleDelete = async (id) => {
         try {
-          await Client.delete(`posts/delete/${id}`)
-          getPosts()
+            await Client.delete(`posts/delete/${id}`)
+            getPosts()
         } catch (error) {
-          throw error
+            throw error
         }
-      }
+    }
 
+    const [formData, setFormData] = useState({
+        userId: `${id}`,
+        image: '',
+        content: ''
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData(prevState => ({ ...prevState, [name]: value }))
+    }
+
+    // const handleSubmit = (id) => async (e) => {
+    //     e.preventDefault()
+    //     console.log(formData)
+    //     try {
+    //         await Client.put(`/posts/edit/${id}`, formData)
+    //     } catch (error) {
+    //         throw error
+    //     }
+    // }
+
+    const handleSubmit = async (id) => {
+        try {
+            await Client.put(`/posts/edit/${id}`, formData)
+        } catch (error) {
+            throw error
+        }
+    }
+
+    const handleFormSubmit = (id) => async (e) => {
+        e.preventDefault()
+        console.log(formData)
+        handleSubmit(id)
+        flipPopup()
+        getPosts()
+    }
 
     useEffect(() => {
         getPosts()
@@ -32,21 +75,43 @@ const MyPosts = () => {
 
     return (
         <div className="bg-[rgb(0,31,63)] w-full h-[100vh] flex flex-wrap justify-center items-center content-center">
+            {popup && (
+                <div className="bg-[rgb(17,122,227)] w-1/2 h-1/2 fixed border-white border-2 flex justify-center items-center">
+                    <div className="text-white w-3/5 h-1/2 items-center flex flex-wrap justify-center">
+                        <form className="flex flex-wrap justify-center" onSubmit={handleFormSubmit(popupInfo)}>
+                            <label className="w-full text-center">
+                                Image URL:
+                            </label>
+                            <input type="text" name="image" value={formData.image} onChange={handleChange} className="w-full text-black" />
+                            <label className="w-full text-center">
+                                Content:
+                            </label>
+                            <textarea name="content" value={formData.content} onChange={handleChange} className=" w-full text-black"></textarea>
+                            <br />
+                            <button type="submit" className=" mt-4 bg-slate-700 p-2">Submit</button>
+                        </form>
+                        <button className="mt-4 bg-slate-700 px-10 py-2" onClick={()=> flipPopup()}>Cancel</button>
+                    </div>
+                </div>
+            )}
             <div className="w-2/3 flex justify-center text-2xl text-white">
                 Your Posts
             </div>
             <div className="w-2/3 h-3/4 flex flex-wrap justify-around overflow-y-scroll">
                 {posts.map((post) => (
                     <div className="bg-[rgb(17,122,227)] text-white w-2/5 h-[550px] mb-8 mt-8 border-4 border-black rounded-lg overflow-hidden">
-                    <img src={post.image} alt="" className=" w-full h-4/5 border-b-4 border-black" />
-                    <div className="h-1/5">
-                        <h1 className=" text-2xl font-bold ml-2">{post.content}</h1>
-                        <div className="ml-2 h-2/3 flex flex-wrap justify-evenly items-end">
-                            <button className=" bg-[rgb(15,77,138)] w-1/5 h-1/2 border-black border-2 cursor-pointer">Edit</button>
-                            <button className=" bg-[rgb(15,77,138)] w-1/5 h-1/2 border-black border-2 cursor-pointer" onClick={()=>handleDelete(post.id)}>Delete</button>
+                        <img src={post.image} alt="" className=" w-full h-4/5 border-b-4 border-black" />
+                        <div className="h-1/5">
+                            <h1 className=" text-2xl font-bold ml-2">{post.content}</h1>
+                            <div className="ml-2 h-2/3 flex flex-wrap justify-evenly items-end">
+                                <button className=" bg-[rgb(15,77,138)] w-1/5 h-1/2 border-black border-2 cursor-pointer" onClick={() => {
+                                    flipPopup()
+                                    setPopupInfo(post.id)
+                                }}>Edit</button>
+                                <button className=" bg-[rgb(15,77,138)] w-1/5 h-1/2 border-black border-2 cursor-pointer" onClick={() => handleDelete(post.id)}>Delete</button>
+                            </div>
                         </div>
                     </div>
-                </div>
                 ))}
             </div>
         </div>
--- controller/PostRouter.js
+++ controller/PostRouter.js
@@ -2,59 +2,71 @@ const { Posts } = require('../models')
 const { Users } = require('../models')
 
 const GetPosts = async (req, res) => {
-    try {
-      const posts = await Posts.findAll({
-        include: { model: Users, as: 'commentAuthor', attributes: ['userName'] }
-      });
-      res.send(posts);
-    } catch (error) {
-      throw error;
-    }
-  };
+  try {
+    const posts = await Posts.findAll({
+      include: { model: Users, as: 'commentAuthor', attributes: ['userName'] }
+    });
+    res.send(posts);
+  } catch (error) {
+    throw error;
+  }
+};
 
 
-const GetPostById = async (req,res) => {
-    try {
-        const { id } = req.params
-        const post = await Posts.findByPk(id)
-        if (post) {
-            return res.status(200).json({post})
-        }
-        return res.status(404).send('Post with the specified ID does not exist')
-    } catch (error) {
-        return res.status(500).send(error.message)
+const GetPostById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const post = await Posts.findByPk(id)
+    if (post) {
+      return res.status(200).json({ post })
     }
+    return res.status(404).send('Post with the specified ID does not exist')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
 }
 
 
 const getPostByUserId = async (req, res) => {
-    try {
-      const {id} = req.params
-      const post = await Posts.findAll({
-        where: {userId: id}
-      })
-      if (post) {
-        return res.status(200).json({ post })
-      }
-      return res.status(404).send('Post with the specified user does not exists')
-    } catch (error) {
-      return res.status(500).send(error.message)
+  try {
+    const { id } = req.params
+    const post = await Posts.findAll({
+      where: { userId: id }
+    })
+    if (post) {
+      return res.status(200).json({ post })
     }
+    return res.status(404).send('Post with the specified user does not exists')
+  } catch (error) {
+    return res.status(500).send(error.message)
   }
+}
 
 
-const CreatePost = async (req,res) => {
-    try {
-        const post = await Posts.create({...req.body})
-        res.send(post)
-    } catch (error) {
-        throw error
-    }
+const CreatePost = async (req, res) => {
+  try {
+    const post = await Posts.create({ ...req.body })
+    res.send(post)
+  } catch (error) {
+    throw error
+  }
+}
+
+const EditPost = async (req, res) => {
+  try {
+    const post = await Posts.update(
+      { ...req.body },
+      { where: { id: req.params.id }, returning: true }
+    )
+    res.send(post)
+  } catch (error) {
+    throw error
+  }
 }
 
 const DeletePost = async (req, res) => {
   try {
-    const {id} = req.params
+    const { id } = req.params
     await Posts.destroy({ where: { id: id } })
     res.send({ msg: 'Meal Removed', payload: id, status: 'Ok' })
   } catch (error) {
@@ -63,9 +75,10 @@ const DeletePost = async (req, res) => {
 }
 
 module.exports = {
-    GetPosts,
-    CreatePost,
-    GetPostById,
-    getPostByUserId,
-    DeletePost
+  GetPosts,
+  CreatePost,
+  GetPostById,
+  getPostByUserId,
+  DeletePost,
+  EditPost
 }
