67796dc62afaecee9404284a206933090d8117e6
Tyler Coats <coatsctyler@gmail.com>
2023-04-04 12:45:51 -0400

    Added navigate to news page

--- client/src/pages/NewsPage.jsx
+++ client/src/pages/NewsPage.jsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react"
 import axios from 'axios'
 import { Link } from "react-router-dom"
 import Client from "../services/api"
+import {useNavigate} from 'react-router-dom'
 
 
 const NewsPage = () => {
 
-    const [articles, setArticles] = useState([])
+    const navigate = useNavigate()
 
+    const [articles, setArticles] = useState([])
+    
     const [singleArticle, setSingleArticle] = useState([])
 
 
@@ -38,14 +41,15 @@ const NewsPage = () => {
                 <div className="w-full h-8 text-xl text-white flex items-center border-b-2 border-white">
                     <h2>Featured Articles</h2>
                 </div>
-                <div className=" bg-[rgba(0,0,0,0.8)] w-1/3 h-4/5 flex flex-wrap justify-center border-2 border-white">
+                <div className=" bg-[rgba(0,0,0,0.8)] w-1/3 h-4/5 flex flex-wrap justify-center border-2 border-white" onClick={()=>navigate(`/article/${singleArticle.id}`)}>
                     <img src={singleArticle.image} alt="" className="w-full h-3/4 object-contain" />
                     <h1 className=" text-xl text-white">{singleArticle.title}</h1>
-                    <Link className=" text-blue-300 w-full text-right"><p>Read More →</p></Link>
+                    <Link to={`/article/${singleArticle.id}`} className=" text-blue-300 w-full text-right"><p>Read More →</p></Link>
                 </div>
-                <div className="h-[95%] w-3/5 ml-6 flex flex-wrap border-2 content-between">
+                <div className="h-[91%] w-3/5 ml-6 flex flex-wrap border-2 content-between">
                     {articles.slice(1, 5).map((article) => (
-                        <div className="w-full h-1/4 flex flex-wrap content-end border-white border-2" style={{
+                        <div className="w-full h-1/4 flex flex-wrap content-end border-white border-2 cursor-pointer" onClick={()=>navigate(`/article/${article.id}`)}
+                        style={{
                             backgroundImage:
                                 `url(${article.image})`,
                             backgroundSize: 'cover',
@@ -63,7 +67,7 @@ const NewsPage = () => {
                 </div>
                 <div className="w-full h-3/4 flex flex-wrap justify-around items-start  content-start overflow-y-scroll bg-[rgb(34,42,51)]">
                         {articles.slice(5).map((article) => (
-                            <div className=" w-[47%] bg-[rgb(95,132,168)] h-16 my-2 flex items-center justify-between mt-6">
+                            <div className=" w-[47%] bg-[rgb(95,132,168)] h-16 my-2 flex items-center justify-between mt-6" onClick={()=>navigate(`/article/${article.id}`)}>
                                 <img src={article.image} alt="" className="h-full w-[140px] mr-2 border-r-2 border-black"/>
                                 <h3 className="flex justify-start mr-2 text-white">{article.title}</h3>
                                 <h4 className="mr-2 text-white">{article.date}</h4>
