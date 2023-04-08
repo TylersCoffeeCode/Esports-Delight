6cffb860a35e8b1068fb24d5c7d8f3d068a68bbd
Tyler Coats <coatsctyler@gmail.com>
2023-04-07 23:41:12 -0400

    Removed css bg test colors

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <nav className="w-full h-16 flex flex-wrap bg-gradient-to-r justify-between from-[rgb(10,10,10)] to-[rgb(1,126,255)] fixed z-10">
-      <div className="w-1/3 bg-red-300 flex">
-        <div className="w-24 bg-green-200 flex justify-center items-center">
+      <div className="w-1/3 flex">
+        <div className="w-24 flex justify-center items-center">
           <Link to='/'><h1 className='text-white text-4xl'>GD</h1></Link>
         </div>
-        <div className='w-full flex justify-around items-center bg-blue-300'>
+        <div className='w-full flex justify-around items-center'>
           <Link to='/allTournaments' className=' text-white'>Tournaments</Link>
           <Link to='/allNews' className=' text-white'>Articles</Link>
           <Link to='/posts' className=' text-white'>Posts</Link>
