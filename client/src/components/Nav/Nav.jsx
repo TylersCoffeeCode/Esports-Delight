c7013d08ac5bffba6a7edd0d30af9d395d9a6125
Tyler Coats <coatsctyler@gmail.com>
2023-04-08 21:25:22 -0400

    Fixed navbar to be more mobile first css

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -3,21 +3,20 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <nav className="w-full h-16 flex flex-wrap bg-gradient-to-r justify-between from-[rgb(10,10,10)] to-[rgb(1,126,255)] fixed z-10">
-      <div className="w-1/3 flex">
-        <div className="w-24 flex justify-center items-center">
+      <div className="w-[83%] flex">
+        <div className="w-18 flex justify-center items-center">
           <Link to='/'><h1 className='text-white text-4xl'>GD</h1></Link>
         </div>
         <div className='w-full flex justify-around items-center'>
-          <Link to='/allTournaments' className=' text-white'>Tournaments</Link>
-          <Link to='/allNews' className=' text-white'>Articles</Link>
-          <Link to='/posts' className=' text-white'>Posts</Link>
+          <Link to='/allTournaments' className='mr-1 text-white'>Tournaments</Link>
+          <Link to='/allNews' className='mr-1 text-white'>Articles</Link>
+          <Link to='/posts' className='mr-1 text-white'>Posts</Link>
         </div>
       </div>
-      <div className="navRight w-1/5 flex items-center justify-end">
-        <div className='w-2/4 h-full flex items-center justify-end'>
-          <div className='flex h-3/4 items-center border-l-2 justify-around w-full'>
-            <Link to='/Account' className=' text-white pl-4'>👤</Link>
-            <Link className='mr-6 text-white'>⚙️</Link>
+      <div className="navRight w-1/6 flex items-center justify-end">
+        <div className='w-full h-full flex items-center justify-end'>
+          <div className='flex h-3/4 items-center border-l-2 justify-center w-full'>
+            <Link to='/Account' className=' text-white'>👤</Link>
           </div>
         </div>
       </div>
