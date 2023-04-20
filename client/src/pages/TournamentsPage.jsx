45a2ef2df1cfd2d82412537a56202164b2b7e4dc
Tyler Coats <coatsctyler@gmail.com>
2023-04-19 23:27:54 -0400

    Added media queries for tournament page

--- client/src/pages/TournamentsPage.jsx
+++ client/src/pages/TournamentsPage.jsx
@@ -24,15 +24,15 @@ const TournamentsPage = () => {
         <div className="bg-[#000000] w-full h-[100vh] flex flex-wrap items-start justify-center">
             <video className=' w-full h-full' src={csVideo} autoPlay muted>
             </video>
-            <div className=" w-1/3 h-5/6 mt-[6rem] flex flex-wrap bg-[rgb(0,31,63)] items-start justify-center rounded-lg fixed">
+            <div className=" w-[90%] h-5/6 mt-[6rem] flex flex-wrap bg-[rgb(0,31,63)] items-start justify-center rounded-lg fixed sm:w-2/3 lg:w-1/3">
                 <div className="w-full h-8 text-2xl text-white flex justify-center mt-4">
                     <h1 className=' font-bold'>Tournaments</h1>
                 </div>
                 <div className=" bg-[rgba(19,49,79,0.44)] w-[95%] h-5/6 border-2 border-black flex flex-wrap justify-center items-start content-start">
                     {tournaments.map((tournament) => (
                         <Link className='w-[98%]' to={`/TournamentDetails/${tournament.id}`}><div className=' bg-[#4c4b64] w-full h-14 mt-2 mb-1 flex items-center justify-between border-2 border-black'>
-                            <h2 className='ml-2 text-white'>{tournament.title}</h2>
-                            <h3 className='mr-2 text-white'>{tournament.date}</h3>
+                            <h2 className='ml-2 text-white text-sm'>{tournament.title}</h2>
+                            <h3 className='mr-2 text-white text-sm'>{tournament.date}</h3>
                         </div></Link>
                     ))}
                 </div>
